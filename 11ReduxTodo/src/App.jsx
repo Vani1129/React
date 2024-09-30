6e38f871e00e566e971eb7040c6ffdaf567493b0
Vani1129 <poojarai1129@gmail.com>
2024-09-30 14:38:23 +0530

    Redux toolkit

---
+++ 11ReduxTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-xl '>Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
