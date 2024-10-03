712b2014db738093a0d4c25d4f02a52d4c93bce2
Vani1129 <poojarai1129@gmail.com>
2024-10-03 18:00:46 +0530

    "Refactored AuthService class: reformatted code, fixed method name casing, and updated error handling"

--- 12MegaProject/src/appwrite/auth.js
+++ 12MegaProject/src/appwrite/auth.js
@@ -1,64 +1,57 @@
 import conf from '../conf/conf.js';
 import {Client, Account, ID} from "appwrite";
 
-export class AuthService{
+export class AuthService {
     client = new Client();
     account;
 
-    constructor(){
+    constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
     }
 
-    async createAccount({email, password,name}){
-        try{
-           const userAccount =  await this.account.create(ID.unique(),email, password,name);
-           if (userAccount){
-                return this.login({email,password});
-            //call another method
-           } else{
-                return userAccount;
-           }
-        } 
-        catch(error){
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.login({email, password});
+                //call another method
+            }
+            return userAccount;
+        } catch (error) {
             throw error;
         }
     }
 
-    async login({email,password}){
-        try{
+    async login({email, password}) {
+        try {
             return await this.account.createEmailSession(email, password);
-         }
-        catch(error){
-             throw error;
-         }
+        } catch (error) {
+            throw error;
+        }
     }
 
-    async getCurrentUSer(){
-        try{
+    // Fixed method name casing
+    async getCurrentUser() {
+        try {
             return await this.account.get();
-         }catch(error){
-            console.log("Appwrite service :: ",error);
-         }
-
-        return  null;
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+            return null;
+        }
     }
 
-    async logout(){
-        try{
+    async logout() {
+        try {
             return await this.account.deleteSessions();
-         }catch(error){
-            console.log("Appwrite service :: logout:: error",error);
-         }
-
-        return  null;
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+            return null;
+        }
     }
 }
 
 const authService = new AuthService();
-
-
 export default authService;
-
